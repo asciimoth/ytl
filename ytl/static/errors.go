9b8ffece57d119c2619fd08b1f9a4caa392df65f
DomesticMoth <silkmoth@protonmail.com>
2022-09-04 01:01:50 +0000

    Implement net.Error for all errors in module

--- ytl/static/errors.go
+++ ytl/static/errors.go
@@ -23,6 +23,10 @@ func (e UnknownSchemeError) Error() string {
 	return fmt.Sprintf("Unknown scheme %s", e.Scheme)
 }
 
+func (e UnknownSchemeError) Timeout() bool { return false }
+
+func (e UnknownSchemeError) Temporary() bool { return false }
+
 type InvalidUriError struct {
 	Err string
 }
@@ -31,6 +35,10 @@ func (e InvalidUriError) Error() string {
 	return fmt.Sprintf("Uri is invalid; %s", e.Err)
 }
 
+func (e InvalidUriError) Timeout() bool { return false }
+
+func (e InvalidUriError) Temporary() bool { return false }
+
 type IvalidPeerPublicKey struct{
 	Text string
 }
@@ -38,6 +46,10 @@ type IvalidPeerPublicKey struct{
 func (e IvalidPeerPublicKey) Error() string {
 	return fmt.Sprintf("Peer public key is invalid; %s", e.Text)
 }
+
+func (e IvalidPeerPublicKey) Timeout() bool { return false }
+
+func (e IvalidPeerPublicKey) Temporary() bool { return false }
 
 type ConnTimeoutError struct {}
 
@@ -60,11 +72,19 @@ func (e InapplicableProxyTypeError) Error() string {
 	return fmt.Sprintf("Proxy type '%s' cannot use with '%s' transport", url.String(), e.Transport)
 }
 
+func (e InapplicableProxyTypeError) Timeout() bool { return false }
+
+func (e InapplicableProxyTypeError) Temporary() bool { return false }
+
 type UnknownProtoError struct {}
 
 func (e UnknownProtoError) Error() string {
 	return fmt.Sprintf("Unknown protocol")
 }
+
+func (e UnknownProtoError) Timeout() bool { return false }
+
+func (e UnknownProtoError) Temporary() bool { return false }
 
 type UnknownProtoVersionError struct {
 	Expected ProtoVersion
@@ -79,6 +99,11 @@ func (e UnknownProtoVersionError) Error() string {
 	)
 }
 
+func (e UnknownProtoVersionError) Timeout() bool { return false }
+
+func (e UnknownProtoVersionError) Temporary() bool { return false }
+
+
 type TransportSecurityCheckError struct {
 	Expected ed25519.PublicKey
 	Received ed25519.PublicKey
@@ -92,11 +117,19 @@ func (e TransportSecurityCheckError) Error() string {
 	)
 }
 
+func (e TransportSecurityCheckError) Timeout() bool { return false }
+
+func (e TransportSecurityCheckError) Temporary() bool { return false }
+
 type ConnClosedByDeduplicatorError struct {}
 
 func (e ConnClosedByDeduplicatorError) Error() string {
 	return fmt.Sprintf("Connection closed by deduplicator")
 }
+
+func (e ConnClosedByDeduplicatorError) Timeout() bool { return false }
+
+func (e ConnClosedByDeduplicatorError) Temporary() bool { return false }
 
 type UnacceptableAddressError struct {
 	Text string
@@ -105,3 +138,8 @@ type UnacceptableAddressError struct {
 func (e UnacceptableAddressError) Error() string {
 	return fmt.Sprintf("Unacceptable address: %s", e.Text)
 }
+
+func (e UnacceptableAddressError) Timeout() bool { return false }
+
+func (e UnacceptableAddressError) Temporary() bool { return false }
+
