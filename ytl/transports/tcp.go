01a059c9c794132f8e056acfb5fc2936419a7322
DomesticMoth <silkmoth@protonmail.com>
2022-09-05 14:40:56 +0000

    Relicence to LGPLv2.1

--- ytl/transports/tcp.go
+++ ytl/transports/tcp.go
@@ -1,11 +1,21 @@
-// Ytl by DomesticMoth
+// Ytl
+// Copyright (C) 2022 DomesticMoth <silkmoth@protonmail.com>
 //
-// To the extent possible under law, the person who associated CC0 with
-// ytl has waived all copyright and related or neighboring rights
-// to ytl.
+// This file is part of Ytl.
 //
-// You should have received a copy of the CC0 legalcode along with this
-// work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+// Ytl is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// Ytl is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 package transports
 
 import(
